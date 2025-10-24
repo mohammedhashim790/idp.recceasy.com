5446db1a0ee16bd80fbd4ee9897f8e483df1375b
Mohammed Hashim <mohammedhashim790@gmail.com>
2025-10-23 21:56:14 -0300

    feat: Updated Login DTO

--- idp/src/main/java/com/recceasy/idp/dto/auth/LoginResponse.java
+++ idp/src/main/java/com/recceasy/idp/dto/auth/LoginResponse.java
@@ -1,14 +1,19 @@
 package com.recceasy.idp.dto.auth;
 
 public class LoginResponse {
-    private String jwt;
-    private String sessionToken;
+    private final String jwt;
+    private final String sessionToken;
 
     public LoginResponse(String jwt, String sessionToken) {
         this.jwt = jwt;
         this.sessionToken = sessionToken;
     }
 
-    public String getJwt() { return jwt; }
-    public String getSessionToken() { return sessionToken; }
+    public String getJwt() {
+        return jwt;
+    }
+
+    public String getSessionToken() {
+        return sessionToken;
+    }
 }
